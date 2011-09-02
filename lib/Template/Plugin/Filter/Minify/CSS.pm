917407ed46f5d6ff79398325968f6ad8a1059b0d
Michael Schout <mschout@gkg.net>
2011-09-02 22:11:02 +0000

    Import of MSCHOUT/Template-Plugin-Filter-Minify-CSS-0.94 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-Filter-Minify-CSS
    gitpan-cpan-version:      0.94
    gitpan-cpan-path:         MSCHOUT/Template-Plugin-Filter-Minify-CSS-0.94.tar.gz
    gitpan-cpan-author:       MSCHOUT
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/Filter/Minify/CSS.pm
+++ lib/Template/Plugin/Filter/Minify/CSS.pm
@@ -1,5 +1,7 @@
 package Template::Plugin::Filter::Minify::CSS;
-our $VERSION = '0.93';
+BEGIN {
+  $Template::Plugin::Filter::Minify::CSS::VERSION = '0.94';
+}
 # ABSTRACT: CSS::Minifier filter for Template Toolkit
 
 use 5.006;
@@ -36,7 +38,7 @@ Template::Plugin::Filter::Minify::CSS - CSS::Minifier filter for Template Toolki
 
 =head1 VERSION
 
-version 0.93
+version 0.94
 
 =head1 SYNOPSIS
 
@@ -59,11 +61,21 @@ filter
 
 =head1 SEE ALSO
 
-L<CSS::Minifer>, L<Template::Plugin::Filter>, L<Template>
+L<CSS::Minifier>, L<Template::Plugin::Filter>, L<Template>
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/mschout/template-plugin-filter-minify-css>
+and may be cloned from L<git://github.com/mschout/template-plugin-filter-minify-css.git>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to bug-template-plugin-filter-minify-css@rt.cpan.org or through the web interface at:
+ http://rt.cpan.org/Public/Dist/Display.html?Name=Template-Plugin-Filter-Minify-CSS
 
 =head1 AUTHOR
 
-  Michael Schout <mschout@cpan.org>
+Michael Schout <mschout@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
@@ -71,19 +83,6 @@ This software is copyright (c) 2009 by Michael Schout.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
-
-=head1 SOURCE
-
-You can contribute or fork this project via github:
-
-http://github.com/mschout/template-plugin-filter-minify-css
-
- git clone git://github.com/mschout/template-plugin-filter-minify-css.git
-
-=head1 BUGS
-
-Please report any bugs or feature requests to bug-template-plugin-filter-minify-css@rt.cpan.org or through the web interface at:
- http://rt.cpan.org/Public/Dist/Display.html?Name=Template-Plugin-Filter-Minify-CSS
 
 =cut
 
